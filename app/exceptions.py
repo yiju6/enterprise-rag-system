9779f62834276e982f737b198755dc1ba6fd6a4f
yiju6 <zoe-juyi@hotmail.com>
2026-03-29 18:56:51 -0400

    feat: add exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,4 +1,11 @@
-
 class UnsupportedFileTypeError(Exception):
     """Raised when no parser can handle the given file."""
+    pass
+
+class UnsupportedBlockTypeError(Exception):
+    """Raised when chunker encounters unsupported block type."""
+    pass
+
+class InvalidPDFFileNameError(Exception):
+    """Raised when company or quarter cannot be parsed from the PDF filename."""
     pass
